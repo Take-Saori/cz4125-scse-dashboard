66a85621370c30191db785e8f8912ded20e8dc7e
Take-Saori <71936501+Take-Saori@users.noreply.github.com>
2023-10-29 05:56:11 +0000

    test

--- pages/4_test.py
+++ pages/4_test.py
@@ -3,4 +3,10 @@ import streamlit as st
 import pandas as pd
 import os
 
-os.listdir()
+def file_selector(folder_path='.'):
+    filenames = os.listdir(folder_path)
+    selected_filename = st.selectbox('Select a file', filenames)
+    return os.path.join(folder_path, selected_filename)
+
+filename = file_selector()
+st.write('You selected `%s`' % filename)
