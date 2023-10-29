028c78042fa20568ded46450bd1681d7dd865b9b
Take-Saori <71936501+Take-Saori@users.noreply.github.com>
2023-10-29 16:40:40 +0800

    test

--- pages/4_test.py
+++ pages/4_test.py
@@ -3,5 +3,5 @@ import pandas as pd
 import os
 
 
-df = pd.read_csv('C:\Users\saori\Desktop\NTU stuff\Courses\Year 4 Sem 1\CZ4125 Developing Data Products\Individual project 1\cz4125-scse-dashboard\updated_csv.csv')
+df = pd.read_csv(f'C:\Users\saori\Desktop\NTU stuff\Courses\Year 4 Sem 1\CZ4125 Developing Data Products\Individual project 1\cz4125-scse-dashboard\updated_csv.csv')
 df.head()
