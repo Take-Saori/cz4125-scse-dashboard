b7672027e3d57ad3d462105bfdfe54e83936d5ba
Take-Saori <71936501+Take-Saori@users.noreply.github.com>
2023-10-29 06:00:03 +0000

    test

--- pages/4_test.py
+++ pages/4_test.py
@@ -1,16 +1,8 @@
 import streamlit as st
 import pandas as pd
+import os
 
 
-try:
-    st.write('Trying with back slashes')
-    st.dataframe(pd.read_csv(r'.\\data\\updated_csv.csv'))
-except:
-    st.write('It didn\'t work with back slashes.')
-
-
-try:
-    st.write('Trying with forward slashes')
-    st.dataframe(pd.read_csv(r'data/updated_csv.csv'))
-except:
-    st.write('It didn\'t work with forward slashes.')
+path = os.path.dirname(__file__)
+my_file = path+'./data/updated_csv.csv'
+print(my_file)
