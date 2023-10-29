77fd67c6025bf43eff0da82241f065a076525e1e
Take-Saori <71936501+Take-Saori@users.noreply.github.com>
2023-10-29 16:48:32 +0800

    test

--- pages/4_test.py
+++ pages/4_test.py
@@ -3,12 +3,6 @@ import pandas as pd
 import os
 
 
-df = pd.DataFrame(
-    [
-        {"command": "st.selectbox", "rating": 4, "is_widget": True},
-        {"command": "st.balloons", "rating": 5, "is_widget": False},
-        {"command": "st.time_input", "rating": 3, "is_widget": True},
-    ]
-)
+df = pd.read_csv('updated_csv.csv')
 
 st.dataframe(df, use_container_width=True)
