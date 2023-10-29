917cd21baafa4c22c709e39244d04bb703cdd2a4
Take-Saori <71936501+Take-Saori@users.noreply.github.com>
2023-10-29 05:47:29 +0000

    test

--- pages/4_test.py
+++ pages/4_test.py
@@ -1,8 +1,6 @@
 
-import streamlit
+import streamlit as st
+import pandas as pd
 
-
-streamlit.image(
-            "https://dr.ntu.edu.sg/cris/rp/fileservice/rp00083/57/?filename=Assoc%20Prof%20A%20S%20Madhukumar%20(2)_1.jpg",
-            width=400, # Manually Adjust the width of the image as per requirement
-        )
+df = pd.read_csv('data/updated_csv.csv')
+df.head()
